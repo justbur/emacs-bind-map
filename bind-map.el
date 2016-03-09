6f84c0254f9ef7580ee32fb66190cc694cc05629
justbur <justin@burkett.cc>
2016-03-09 08:25:59 -0500

    Increment version

--- bind-map.el
+++ bind-map.el
@@ -4,7 +4,7 @@
 
 ;; Author: Justin Burkett <justin@burkett.cc>
 ;; URL: https://github.com/justbur/emacs-bind-map
-;; Version: 1.0.3
+;; Version: 1.0.4
 ;; Keywords:
 ;; Package-Requires: ((emacs "24.3"))
 
